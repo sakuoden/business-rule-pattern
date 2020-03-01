357bbda82ae09e4e66373b0ff493ad1ca1348d10
作佐部 太祐 <sakusabedaisuke@team-lab.com>
2020-03-01 18:11:12 +0900

    [add] enum object "FeeType"

---
+++ src/main/kotlin/com/jackthenewest/businessrulepattern/domain/model/fee/FeeType.kt
@@ -0,0 +1,16 @@
+package com.jackthenewest.businessrulepattern.domain.model.fee
+
+import com.jackthenewest.businessrulepattern.domain.type.yen.Yen
+
+/**
+ * 料金種別
+ */
+enum class FeeType(
+  private val fee: Fee
+) {
+  SENIOR(SeniorFee()),
+  ADULT(AdultFee()),
+  CHILD(ChildFee());
+
+  fun yen(): Yen = fee.yen()
+}
