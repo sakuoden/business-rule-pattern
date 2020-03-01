f6a9cfbe852d64ecd34d56e5c16829bdb038a1c1
作佐部 太祐 <sakusabedaisuke@team-lab.com>
2020-03-01 15:26:46 +0900

    first commit

---
+++ src/main/kotlin/com/jackthenewest/businessrulepattern/BusinessRulePatternApplication.kt
@@ -0,0 +1,11 @@
+package com.jackthenewest.businessrulepattern
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BusinessRulePatternApplication
+
+fun main(args: Array<String>) {
+    runApplication<BusinessRulePatternApplication>(*args)
+}
---
+++ src/test/kotlin/com/jackthenewest/businessrulepattern/BusinessRulePatternApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.jackthenewest.businessrulepattern
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BusinessRulePatternApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
