ee38575670ad3b77c63d6ece70d65593817d8ff2
作佐部 太祐 <sakusabedaisuke@team-lab.com>
2020-03-01 18:10:48 +0900

    [update] add senior in CustomerType1

--- src/main/kotlin/com/jackthenewest/businessrulepattern/domain/model/CustomerType.kt
+++
@@ -1,9 +0,0 @@
-package com.jackthenewest.businessrulepattern.domain.model
-
-/**
- * 顧客区分
- */
-enum class CustomerType {
-  ADULT,
-  CHILD
-}
---
+++ src/main/kotlin/com/jackthenewest/businessrulepattern/domain/model/customer/CustomerType.kt
@@ -0,0 +1,10 @@
+package com.jackthenewest.businessrulepattern.domain.model.customer
+
+/**
+ * 顧客区分
+ */
+enum class CustomerType {
+  SENIOR,
+  ADULT,
+  CHILD
+}
